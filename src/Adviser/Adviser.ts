34ffc9d7c3dbe047af84e46ee1f942bd8636358d
Matias Risco <matiasriscolagos@gmail.com>
2023-10-17 23:08:39 +0200

    Add class Adviser

---
+++ src/Adviser/Adviser.ts
@@ -0,0 +1,17 @@
+import Character from "../Character/Character";
+import { type AdviserData } from "../Type";
+
+class Adviser extends Character {
+  adviseCharacter;
+
+  constructor(data: AdviserData) {
+    super(data);
+    this.adviseCharacter = data.adviseCharacter;
+  }
+
+  communicate() {
+    return "No sé por qué, pero creo que voy a morir pronto";
+  }
+}
+
+export default Adviser;
--- src/Type.ts
+++ src/Type.ts
@@ -1,3 +1,5 @@
+import type Character from "./Character/Character";
+
 export interface CharacterData {
   name: string;
   family: string;
@@ -11,4 +13,8 @@ export interface KingData extends CharacterData {
 export interface FighterData extends CharacterData {
   weapon: string;
   dexterity: number;
+}
+
+export interface AdviserData extends CharacterData {
+  adviseCharacter: Character;
 }

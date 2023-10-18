a6d16fab9895d45f43e469a950c2593e25bfd07d
Matias Risco <matiasriscolagos@gmail.com>
2023-10-18 19:25:13 +0200

    Add component Card of ts

---
+++ src/components/Card/Card.ts
@@ -0,0 +1,35 @@
+import type Character from "../../classes/Character/Character";
+import Component from "../Component";
+
+class Card extends Component {
+  constructor(
+    parentElement: HTMLElement,
+    private readonly data: Character,
+  ) {
+    super(parentElement, "article", "character");
+  }
+
+  protected populate(): void {
+    const { name, family, age, alive, img } = this.data;
+
+    this.element.innerHTML = `
+      <div class="card character__card">
+        <img src="img/${img}" alt="${name} ${family}" class="character__picture card-img-top" />
+        <div class="card-body">
+          <h2 class="character__name card-title h4">${name} ${family}</h2>
+          <div class="character__info">
+            <ul class="list-unstyled">
+              <li>Age: ${age} yrs</li>
+              <li>
+                State:
+                <i class="fas fa-thumbs-${alive ? "up" : "down"}"></i>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    `;
+  }
+}
+
+export default Card;
